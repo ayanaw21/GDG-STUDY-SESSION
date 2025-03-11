f96ce287c2b43ac85d77e866f1328da5356d0deb
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-03-11 00:19:25 -0700

    final-commit

--- Task-Manager/src/App.jsx
+++ Task-Manager/src/App.jsx
@@ -3,12 +3,11 @@ import { ToastContainer } from "react-toastify";
 import Form from "./Form";
 import Tasks from "./Tasks";
 
-
 const App = () => {
 	return (
 		<section className="section-center">
 			<ToastContainer position="top-center" />
-      <Tasks/>
+			<Tasks />
 			<Form />
 		</section>
 	);
